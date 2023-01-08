5fa5a41e81899ef039f0453d50463000c8479db9
Brad Johnson <bradsk88@gmail.com>
2023-01-08 15:37:45 -0600

    Account export working

--- src/content_scripts/accounts.tsx
+++ src/content_scripts/accounts.tsx
@@ -1,12 +1,46 @@
-import {AccountStore} from "firefly-iii-typescript-sdk-fetch/dist/models";
+import {
+    AccountRoleProperty,
+    AccountStore,
+    ShortAccountTypeProperty
+} from "firefly-iii-typescript-sdk-fetch/dist/models";
 
-function scrapeAccountsFromPage(): AccountStore[] {
-    // TODO: This is where you implement the scraper to pull the individual
-    //  accounts from the page
-    return [];
+const knownAccountsWithoutNumbers: {[key: string]: string} = {
+    'TFSA GICs': 'all_tfsa_gics',
+    'TFSA Savings Account': 'tfsa_savings',
 }
 
-window.addEventListener("load",function(event) {
+function scrapeAccountsFromPage(): AccountStore[] {
+    expandAll();
+    return Array.from(document.querySelectorAll('li.account.accordion-item').values()).map(
+        row => {
+            let infoSection = row.querySelector('.account__details');
+            let accountNumber = infoSection!.id?.replace('-account', '')?.trim();
+            let nameSection = infoSection!.querySelector('span.account__details-name');
+            let accountName = nameSection?.textContent?.trim();
+            if  (!accountName) {
+                accountName = infoSection!.textContent!;
+            }
+            let name = accountName.trim();
+            if (!accountNumber) {
+                accountNumber = knownAccountsWithoutNumbers[name];
+            }
+            return {
+                name: `${name} (EQ Bank)`,
+                type: ShortAccountTypeProperty.Asset,
+                accountNumber: accountNumber,
+                accountRole: AccountRoleProperty.SavingAsset, // TODO: Infer this from the page headers
+            };
+        }
+    )
+}
+
+function expandAll(): void {
+    document.querySelectorAll('button.accordion-item[aria-expanded=false]').forEach(
+        e => (e as HTMLButtonElement).click()
+    )
+}
+
+window.addEventListener("load", function (event) {
     const button = document.createElement("button");
     button.textContent = "Export accounts to Firefly III"
     button.addEventListener("click", () => {
@@ -16,7 +50,8 @@ window.addEventListener("load",function(event) {
                 action: "store_accounts",
                 value: accounts,
             },
-            () => {}
+            () => {
+            }
         );
     }, false);
 
@@ -30,7 +65,7 @@ window.addEventListener("load",function(event) {
     const housing = document.createElement("div")
     housing.style.textAlign = "end";
     housing.style.width = "100%";
-    housing.style.marginBlock = "-37px";
+    // housing.style.marginBlock = "-37px";
     housing.append(button)
 
     setTimeout(() => {
